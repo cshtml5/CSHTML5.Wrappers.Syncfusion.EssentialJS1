28dc57881482720c3f28b84f3965ba8a9ac80fdf
CSHTML5 <contact@cshtml5.com>
2019-02-22 18:40:12 +0100

    Move all the nuspec files into the same folder and automate NuGet package generation via a .bat file + add "NuGet.exe" to the "ExtLibs" folder of the solution + Update NuGet packages version to "1.0.0-alpha01-002" + Add CSHTML5 as a package dependency to all the projects.

---
+++ NUGET_PACKAGES/Build NuGet packages.bat
@@ -0,0 +1,7 @@
+..\ExtLibs\NuGet\nuget.exe pack "CSHTML5.Migration.Wrappers.Syncfusion.EssentialJS1.Common.nuspec"
+..\ExtLibs\NuGet\nuget.exe pack "CSHTML5.Migration.Wrappers.Syncfusion.EssentialJS1.RichTextEditor.nuspec"
+..\ExtLibs\NuGet\nuget.exe pack "CSHTML5.Migration.Wrappers.Syncfusion.EssentialJS1.SpreadSheet.nuspec"
+..\ExtLibs\NuGet\nuget.exe pack "CSHTML5.Wrappers.Syncfusion.EssentialJS1.Common.nuspec"
+..\ExtLibs\NuGet\nuget.exe pack "CSHTML5.Wrappers.Syncfusion.EssentialJS1.RichTextEditor.nuspec"
+..\ExtLibs\NuGet\nuget.exe pack "CSHTML5.Wrappers.Syncfusion.EssentialJS1.SpreadSheet.nuspec"
+pause
